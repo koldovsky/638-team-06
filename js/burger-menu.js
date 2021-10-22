9cead9f288f836da3db595bcbab75b47d8f1eaff
kateryna1303 <firmastep@ukr.net>
2021-10-22 16:57:14 +0300

    changes

---
+++ js/burger-menu.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector(".menu-icon");
+if (iconMenu) {
+    const menuBody = document.querySelector(".menu");
+    iconMenu.addEventListener("click", function(e) {
+        document.body.classList.toggle("_lock");
+        iconMenu.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    });
+}
