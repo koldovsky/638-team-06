893a302a3c567b5f30bd83ca681ff48e4501ebaf
AntoninaAnd <toshka9@gmail.com>
2021-10-24 21:06:36 +0300

    Carousel

---
+++ js/carousel.js
@@ -0,0 +1,63 @@
+(function () {
+    const slides = [
+        ` <p class="reviews">"I really like to please myself with good wine with a rich taste.
+        Therefore,
+        from
+        time tj time, I
+        order wine directly from the CaliWines winery. I also buy Chardonnay and
+        Cabarnet for family
+        holidays. We are completely delighted with the amazing taste of these wines,
+        which we want
+        to
+        enjoy
+        endlessly"
+        </p>`,
+        `<p class="reviews">"We were to hold a responsible event with a buffet table and
+        wanted to
+        surprise
+        our
+        guests with some
+        local products. I recalled that we have the CaliWines winary in California and
+        ordered 10
+        bottles of
+        Chardonnay. Our guests were delighted and asked for the name of the producer!"
+        </p>`,
+        `<p class="reviews">"Our restaurant has been cooperating with this winery for
+        over 10 years. We
+        order
+        the
+        whole range of
+        wine from them. We are completely satisfied with the quality of the wine
+        supplied to us. And
+        our
+        guests often choose CaliWines from our wine menu."
+        </p>`,
+    ]
+
+    let currentSlide = 0;
+
+    function showCurrentSlide() {
+        const slideContainer = document.querySelector(`.carousel .slide`);
+        slideContainer.innerHTML = slides[currentSlide];
+    }
+
+    function nextSlide() {
+        currentSlide++;
+        if (currentSlide >= slides.length) currentSlide = 0;
+        showCurrentSlide();
+    }
+
+    function prevSlide() {
+        currentSlide--;
+        if (currentSlide < 0) currentSlide = slide.length -1;
+        showCurrentSlide();
+    }
+
+    setInterval(nextSlide, 1000);
+
+    document.querySelector(`.carousel .next`).addAventListener(`click`, nextSlide);
+    document.querySelector(`.carousel .prev`).addAventListener(`click`, prevSlide);
+    
+    showCurrentSlide();
+
+})();
