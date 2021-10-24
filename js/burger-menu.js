d997e50c2fb9eb09de4c883dfa66f7f07aff67bf
kateryna1303 <firmastep@ukr.net>
2021-10-24 21:36:29 +0300

    wine traditional fragment on the main page

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,8 +1,13 @@
-const iconMenu = document.querySelector(".menu-icon");
+(function() {
+    const iconMenu = document.querySelector(".menu-icon");
 
-    const menuBody = document.querySelector(".menu");
-    iconMenu.addEventListener("click", function(e) {
-        document.body.classList.toggle("_lock");
-        iconMenu.classList.toggle("_active");
-        menuBody.classList.toggle("_active");
+        const menuBody = document.querySelector(".menu");
+        iconMenu.addEventListener("click", function(e) {
+            document.body.classList.toggle("_lock");
+            iconMenu.classList.toggle("_active");
+            menuBody.classList.toggle("_active");
     });
+})();
+
+
+
