1f0512953f5289c0622b1a09eecc7ceced5d0c50
Fushtei Sviatoslav <fuschtei@gmail.com>
2021-10-31 07:37:49 +0200

    for (our wines) added transision on information page

--- js/age-check.js
+++ js/age-check.js
@@ -18,58 +18,42 @@ lessThan21.addEventListener('click', lessThan21function, { 'once': true });
 (async function () {
     const responce = await fetch('wine-products.json');
     const productsOurWines = await responce.json();
-        //[
-        //     {
-        //         id: "1",
-        //         title: 'Pinot noir',
-        //         price: 12.00,
-        //         image: 'img/imagine-wine-bottle/pinot-noir.jpeg',
-        //         classForCssAndHrefAndImg: 'pinot-noir'
-        //     },
 
-        //     {
-        //         id: "2",
-        //         title: 'Riesling',
-        //         price: 14.00,
-        //         image: 'img/imagine-wine-bottle/riesling.jpeg',
-        //         classForCssAndHrefAndImg: 'riesling'
-        //     },
-
-        //     {
-        //         id: "3",
-        //         title: 'Zifandel',
-        //         price: 16.00,
-        //         image: 'img/imagine-wine-bottle/zifandel.jpeg',
-        //         classForCssAndHrefAndImg: 'zifandel'
-        //     },
-
-        //     {
-        //         id: "4",
-        //         title: 'Soave',
-        //         price: 12.00,
-        //         image: 'img/imagine-wine-bottle/soave.jpeg',
-        //         classForCssAndHrefAndImg: 'soave'
-        //     }
-        // ];
-
-        function renderProductsOurWines(productsOurWines) {
-            const productsConatainer = document.querySelector('.assortment');
-            for (const product of productsOurWines) {
-                productsConatainer.innerHTML +=
-                    `
-            <div class="card-wine-shop ${product.classForCssAndHrefAndImg}-assortment-index">
-                <a href="wine-separate-page.html">
-                    <img class="assortment-bottle-image" src="${product.image}"
-                        alt="imagine bottle of ${product.image}">
-                </a>
-                <span class="name-wine-font">${product.title}</span>
-                <span class="price-font">${product.price.toFixed(2)} USD</span>
-                <input class="button-add-to-card" type="button" value="Add to cart">
-            </div>
-            `
-            }
+    function renderProductsOurWines(productsOurWines) {
+        const productsConatainer = document.querySelector('.assortment');
+        const filterProductsContainer = productsOurWines.filter(wine => ["3", "4", "8", "6"].includes(wine.id));
+        for (const product of filterProductsContainer) {
+            productsConatainer.innerHTML +=
+                `
+                <div class="card-wine-shop ${product.classForCssAndHrefAndImg}-assortment-index">
+                    <a class="image-bottle-to-info" href="wine-separate-page.html" data-id="${product.id}">
+                        <img class="assortment-bottle-image" src="${product.image}"
+                            alt="imagine bottle of ${product.title}">
+                    </a>
+                    <span class="name-wine-font">${product.title}</span>
+                    <span class="price-font">${product.price.toFixed(2)} USD</span>
+                    <input class="button-add-to-card" type="button" value="Add to cart">
+                </div>
+                `
         };
-
+        //wine-separate-page.html
+        // const imageBottleToInfofunction = () => alert("3");
+        // infoWineImage.addEventListener('click', imageBottleToInfofunction);
+    };
+    // const infoWineImage = document.querySelector('.image-bottle-to-info');
     renderProductsOurWines(productsOurWines);
 
+    document.querySelectorAll('.image-bottle-to-info')
+        .forEach(wineInfoButton => wineInfoButton.addEventListener('click', wineInfoClick));
+
+    window.open(a);
+
+    function wineInfoClick(event) {
+        const bottleInfoButton = event.currentTarget;
+        const productId = bottleInfoButton.dataset.id;
+        const product = productsOurWines.filter(product => product.id === productId)[0];
+        localStorage.wineProduct = JSON.stringify(product);
+    };
+
+    // const imageBottleToInfofunction = () => localStorage.wineProduct = JSON.stringify("product.id");
 })();
